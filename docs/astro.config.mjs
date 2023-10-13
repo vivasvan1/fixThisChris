376cc04d32ab05b8b792d6dce5cd2e5fc1f69fa7
Vivasvan Patel <vivasvanpatel40@gmail.com>
2023-10-13 08:49:36 +0530

    adding base and site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,30 +1,32 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'fixThisChris',
-			social: {
-				github: 'https://github.com/vivasvan1/fixThisChris',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "fixThisChris",
+      social: {
+        github: "https://github.com/vivasvan1/fixThisChris",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+  site: "https://vivasvan1.github.io",
+  base: "/fixThisChris",
 });
